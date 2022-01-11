51a5383155b4f42e5a707c012c55acf820f1eda7
rut.bastoni@gmail.com <dunkelblau>
2022-01-11 17:46:26 +0100

    Android - scansione foto sfocate (OpenCv)

--- src/android/BlurDetectPlugin.java
+++ src/android/BlurDetectPlugin.java
@@ -1,13 +1,12 @@
 package org.apache.cordova.blurdetect;
 
-import org.apache.cordova.PermissionHelper;
+import org.json.JSONObject;
 import org.opencv.android.OpenCVLoader;
 import org.opencv.android.Utils;
 import org.opencv.core.Core;
 import org.opencv.core.CvType;
 import org.opencv.core.Mat;
 import org.opencv.core.MatOfDouble;
-import org.opencv.imgcodecs.Imgcodecs;
 import org.opencv.imgproc.Imgproc;
 
 import org.apache.cordova.CallbackContext;
@@ -22,7 +21,7 @@ import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 
-import java.io.File;
+import java.text.DecimalFormat;
 
 /**
  * This class exposes methods in Cordova that can be called from JavaScript.
@@ -42,50 +41,39 @@ public class BlurDetectPlugin extends CordovaPlugin {
      */
     @SuppressLint("NewApi")
     public boolean execute(String action, final JSONArray args, final CallbackContext callbackContext) throws JSONException {
-        OpenCVLoader.initDebug();
-        this.cordova.requestPermissions(this, 0, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE});
-        String imageUri = args.getString(0);
-        _imageUri = imageUri;
-        _callbackContext = callbackContext;
         if (action.equals("checkImage")) {
-            imageProcess();
-        } else {
-            return false;
+            OpenCVLoader.initDebug();
+
+            JSONObject obj = new JSONObject(args.getString(0));
+            String imageUri = obj.getString("uri");
+            _imageUri = imageUri;
+            _callbackContext = callbackContext;
+            this.cordova.requestPermissions(this, 0, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE});
+//            imageProcess(); Lo chiama direttamente onPermissionRequestResult
+            return true;
         }
-        return true;
+        return false;
     }
 
     private void imageProcess() {
-        Mat image = Imgcodecs.imread(_imageUri, Imgcodecs.CV_LOAD_IMAGE_COLOR);
-        if (image.empty()) {
+        Bitmap originalBmp = BitmapFactory.decodeFile(_imageUri);
+        if (originalBmp == null) {
             _callbackContext.error("CANNOT OPEN IMAGE!");
         } else {
             Mat destination = new Mat();
             Mat matGray = new Mat();
-            Mat kernel = new Mat(3, 3, CvType.CV_32F) {
-                {
-                    put(0, 0, 0.0);
-                    put(0, 1, -1.0);
-                    put(0, 2, 0.0);
-
-                    put(1, 0, -1.0);
-                    put(1, 1, 4.0);
-                    put(1, 2, -1.0);
-
-                    put(2, 0, 0.0);
-                    put(2, 1, -1.0);
-                    put(2, 2, 0.0);
-                }
-            };
+            Mat image = new Mat();
+            Utils.bitmapToMat(originalBmp, image);
             Imgproc.cvtColor(image, matGray, Imgproc.COLOR_BGR2GRAY);
-            Imgproc.filter2D(matGray, destination, -1, kernel);
+            Imgproc.Laplacian(matGray, destination, CvType.CV_64F);
             MatOfDouble median = new MatOfDouble();
             MatOfDouble std = new MatOfDouble();
             Core.meanStdDev(destination, median, std);
-
             double result = Math.pow(std.get(0, 0)[0], 2.0);
-            boolean isBlur = Math.round(result) > 85;
-            _callbackContext.success(isBlur ? "BLUR" : "NOT BLUR");
+
+            //return DecimalFormat("0.00").format(Math.pow(std.get(0, 0)[0], 2.0)).toDouble()
+            String textResult = (new DecimalFormat("0.00")).format(result);
+            _callbackContext.success(textResult);            
         }
     }
 
@@ -99,7 +87,12 @@ public class BlurDetectPlugin extends CordovaPlugin {
 
         switch (requestCode) {
             case WRITE_EXTERNAL_STORAGE_PERMISSION:
-                imageProcess();
+                //Do not block the core thread
+                cordova.getThreadPool().execute(new Runnable() {
+                    public void run() {
+                        imageProcess();
+                    }
+                });
                 break;
         }
     }
